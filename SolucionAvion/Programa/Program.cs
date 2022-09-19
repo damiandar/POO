78b2508dc2e7843973891116310628e62d1a1031
damianrossoistea <damiandar@hotmail.com>
2022-09-18 23:54:04 -0300

    Avion

---
+++ SolucionAvion/Programa/Program.cs
@@ -0,0 +1,17 @@
+﻿using Modelos;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var avion=new Avion();
+
+var azafata1=new Tcp(111,"Florencia","Diaz");
+var azafata2=new Tcp(222,"Carla","Barcos");
+
+avion.Abordar(azafata1);
+avion.Abordar(azafata2);
+
+var piloto=new Piloto(333,"Laura","Alonso");
+avion.Abordar(piloto);
+
+avion.Reporte();
+
